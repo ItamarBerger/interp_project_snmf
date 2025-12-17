1533940679c085fd2f9d31a6afb237536d1f11ef
Tslil Shani <tslilshani@mail.tau.ac.il>
2025-12-17 12:03:34 +0200

    initial slurm setup and test files

---
+++ experiments/test_snmf_steering.sh
@@ -0,0 +1,17 @@
+PYTHONPATH=. python experiments/train/train.py \
+    --sparsity 0.01 \
+    --ranks 50 \
+    --max-iterations-per-layer 2000 \
+    --patience 1500 \
+    --model-name gpt2-small \
+    --factorization-mode mlp \
+    --layers 0 \
+    --data-path data/final_dataset_20_concepts.json \
+    --model-device cuda \
+    --data-device cpu \
+    --fitting-device cuda \
+    --base-path . \
+    --save-path experiments/artifacts/ \
+    --seed 42
+
+echo "Finished."
