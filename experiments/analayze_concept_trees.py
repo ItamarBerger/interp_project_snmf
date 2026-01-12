abb73bc624fc9982db5ef9f817baa2820b73ff92
itamarberger <itamarberger@mail.tau.ac.il>
2026-01-12 18:51:37 +0200

    fixed base path issue in analaysis

--- experiments/analayze_concept_trees.py
+++ experiments/analayze_concept_trees.py
@@ -1,12 +1,11 @@
 import argparse
 import pickle
 import os
-
+import sys
 
 import logging
 
-
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 logger = logging.getLogger(__name__)
 
