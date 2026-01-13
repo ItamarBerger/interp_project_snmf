1f42cea46ddc6771caa38e5cf7cf1d598cacae0c
Itamar Berger <Itamark951@gmail.com>
2026-01-13 13:45:29 +0200

    created table creation pipeline for concept trees analasis

--- experiments/run_analayze_concept_trees.sh
+++ experiments/run_analayze_concept_trees.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# export PYTHONPATH=.
+#  python experiments/analayze_concept_trees.py \
+#   --factorization-base-path experiments/artifacts/hier/ \
+#   --model-name gpt2-small \
+#   --layers 0,3,6,9,11 \
+#   --ranks 400,200,100,50 \
+#   --output-path experiments/artifacts/concept_trees/
+
+
 export PYTHONPATH=.
- python experiments/analayze_concept_trees.py \
-  --factorization-base-path experiments/artifacts/hier/ \
-  --model-name gpt2-small \
+ python experiments/visualize_concept_trees_analaysis.py \
   --layers 0,3,6,9,11 \
-  --ranks 400,200,100,50 \
-  --output-path experiments/artifacts/concept_trees/
+  --concept-trees-analysis-path experiments/artifacts/concept_trees/concept_trees_analysis \
+  --output-path experiments/artifacts/concept_trees/concept_trees_analysis_visualizations/
